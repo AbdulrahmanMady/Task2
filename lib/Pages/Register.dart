1340f0ea401be325a01dff81328e9843a8d79909
AbdoMady2002 <abdomady197@gmail.com>
2023-03-30 19:28:24 +0200

    add register page

---
+++ lib/Pages/Register.dart
@@ -0,0 +1,71 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+import '../Shared/Constans.dart';
+import '../Shared/colors.dart';
+
+class Register extends StatelessWidget {
+  const Register({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(33.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 70,
+              ),
+              TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  obscureText: false,
+                  decoration: widget_text_felid.copyWith(
+                    hintText: "Enter your email",
+                  )),
+              const SizedBox(height: 30),
+              TextField(
+                  keyboardType: TextInputType.text,
+                  obscureText: true,
+                  decoration: widget_text_felid.copyWith(
+                    hintText: "Enter your password",
+                  )),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(BTNgreen),
+                  padding: MaterialStateProperty.all(const EdgeInsets.all(12)),
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8))),
+                ),
+                child: const Text(
+                  "Sign in",
+                  style: TextStyle(fontSize: 19),
+                ),
+              ),
+              const SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Do not have an account ?",
+                      style: TextStyle(fontSize: 20)),
+                  TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "ٍٍSign Up",
+                        style: TextStyle(color: Colors.black),
+                      ))
+                ],
+              )
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tsk2/Pages/Login.dart';
+
+import 'Pages/Register.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -9,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: testttttt(),
+      home: Register(),
     );
   }
 }
