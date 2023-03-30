4046bb5b396f170261f1a2650ebd003c68994096
AbdoMady2002 <abdomady197@gmail.com>
2023-03-30 19:24:34 +0200

    add safe area

--- lib/Pages/Login.dart
+++ lib/Pages/Login.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 
-
 import '../Shared/Constans.dart';
 import '../Shared/colors.dart';
 
@@ -11,41 +10,59 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 200,
-          ),
-          TextField(
-              keyboardType: TextInputType.emailAddress,
-              obscureText: false,
-              decoration: widget_text_felid.copyWith(
-                hintText: "Enter your email",
-              )),
-          const SizedBox(height: 30),
-          TextField(
-              keyboardType: TextInputType.text,
-              obscureText: true,
-              decoration: widget_text_felid.copyWith(
-                hintText: "Enter your password",
-              )),
-          ElevatedButton(
-            onPressed: () {},
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(BTNgreen),
-              padding: MaterialStateProperty.all(EdgeInsets.all(12)),
-              shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8))),
+    return SafeArea(
+      child: Scaffold(
+          body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 70,
             ),
-            child: Text(
-              "Sign in",
-              style: TextStyle(fontSize: 19),
+            TextField(
+                keyboardType: TextInputType.emailAddress,
+                obscureText: false,
+                decoration: widget_text_felid.copyWith(
+                  hintText: "Enter your email",
+                )),
+            const SizedBox(height: 30),
+            TextField(
+                keyboardType: TextInputType.text,
+                obscureText: true,
+                decoration: widget_text_felid.copyWith(
+                  hintText: "Enter your password",
+                )),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(BTNgreen),
+                padding: MaterialStateProperty.all(const EdgeInsets.all(12)),
+                shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8))),
+              ),
+              child: const Text(
+                "Sign in",
+                style: TextStyle(fontSize: 19),
+              ),
             ),
-          ),
-        ],
-      ),
-    ));
+            const SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Do not have an account ?",
+                    style: TextStyle(fontSize: 20)),
+                TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "ٍٍSign Up",
+                      style: TextStyle(color: Colors.black),
+                    ))
+              ],
+            )
+          ],
+        ),
+      )),
+    );
   }
 }
