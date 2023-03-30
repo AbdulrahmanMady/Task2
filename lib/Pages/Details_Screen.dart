4dbf0bf6ca03383593f0adda4562609aa26deea8
AbdoMady2002 <abdomady197@gmail.com>
2023-03-30 23:36:45 +0200

    add text button to page

--- lib/Pages/Details_Screen.dart
+++ lib/Pages/Details_Screen.dart
@@ -6,8 +6,13 @@ import 'package:flutter/src/widgets/placeholder.dart';
 
 import '../Shared/colors.dart';
 
-class Details extends StatelessWidget {
-  const Details({super.key});
+class Details extends StatefulWidget {
+  @override
+  State<Details> createState() => _DetailsState();
+}
+
+class _DetailsState extends State<Details> {
+  bool isShowMore = true;
 
   @override
   Widget build(BuildContext context) {
@@ -43,92 +48,114 @@ class Details extends StatelessWidget {
         backgroundColor: appbarGreen,
         title: Text("Home"),
       ),
-      body: Column(
-        children: [
-          Image.asset("assets/img/1.webp"),
-          SizedBox(
-            height: 11,
-          ),
-          Text(
-            "\$ 12.99 ",
-            style: TextStyle(fontSize: 20),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Container(
-                padding: EdgeInsets.all(4),
-                child: Text(
-                  "New",
-                  style: TextStyle(fontSize: 12),
-                ),
-                decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 255, 129, 129),
-                    borderRadius: BorderRadius.circular(4)),
-              ),
-              Row(
-                children: [
-                  Icon(
-                    Icons.star,
-                    size: 26,
-                    color: Color.fromARGB(255, 255, 191, 0),
-                  ),
-                  Icon(
-                    Icons.star,
-                    size: 26,
-                    color: Color.fromARGB(255, 255, 191, 0),
-                  ),
-                  Icon(
-                    Icons.star,
-                    size: 26,
-                    color: Color.fromARGB(255, 255, 191, 0),
-                  ),
-                  Icon(
-                    Icons.star,
-                    size: 26,
-                    color: Color.fromARGB(255, 255, 191, 0),
-                  ),
-                  Icon(
-                    Icons.star,
-                    size: 26,
-                    color: Color.fromARGB(255, 255, 191, 0),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Icon(
-                    Icons.edit_location,
-                    size: 26,
-                    color: Color.fromARGB(168, 3, 65, 27),
-                    // color: Color.fromARGB(255, 186, 30, 30),
-                  ),
-                  SizedBox(
-                    width: 3,
-                  ),
-                  Text(
-                    "Flower Shop",
-                    style: TextStyle(fontSize: 19),
-                  ),
-                ],
-              )
-            ],
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          SizedBox(
-            width: double.infinity,
-            child: Text(
-              "Details : ",
-              style: TextStyle(fontSize: 22),
-              textAlign: TextAlign.start,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset("assets/img/1.webp"),
+            SizedBox(
+              height: 11,
             ),
-          ),
-          SizedBox(
-            height: 16,
-          ),
-        ],
+            Text(
+              "\$ 12.99 ",
+              style: TextStyle(fontSize: 20),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(4),
+                  child: Text(
+                    "New",
+                    style: TextStyle(fontSize: 12),
+                  ),
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 255, 129, 129),
+                      borderRadius: BorderRadius.circular(4)),
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.star,
+                      size: 26,
+                      color: Color.fromARGB(255, 255, 191, 0),
+                    ),
+                    Icon(
+                      Icons.star,
+                      size: 26,
+                      color: Color.fromARGB(255, 255, 191, 0),
+                    ),
+                    Icon(
+                      Icons.star,
+                      size: 26,
+                      color: Color.fromARGB(255, 255, 191, 0),
+                    ),
+                    Icon(
+                      Icons.star,
+                      size: 26,
+                      color: Color.fromARGB(255, 255, 191, 0),
+                    ),
+                    Icon(
+                      Icons.star,
+                      size: 26,
+                      color: Color.fromARGB(255, 255, 191, 0),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.edit_location,
+                      size: 26,
+                      color: Color.fromARGB(168, 3, 65, 27),
+                      // color: Color.fromARGB(255, 186, 30, 30),
+                    ),
+                    SizedBox(
+                      width: 3,
+                    ),
+                    Text(
+                      "Flower Shop",
+                      style: TextStyle(fontSize: 19),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: Text(
+                "Details : ",
+                style: TextStyle(fontSize: 22),
+                textAlign: TextAlign.start,
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Text(
+              "A flower, sometimes known as a bloom or blossom, is the reproductive structure found in flowering plants (plants of the division Angiospermae). The biological function of a flower is to facilitate reproduction, usually by providing a mechanism for the union of sperm with eggs. Flowers may facilitate outcrossing (fusion of sperm and eggs from different individuals in a population) resulting from cross-pollination or allow selfing (fusion of sperm and egg from the same flower) when self-pollination occurs.",
+              style: TextStyle(
+                fontSize: 18,
+              ),
+              maxLines: isShowMore ? 3 : null,
+              overflow: TextOverflow.fade,
+            ),
+            TextButton(
+                onPressed: () {
+                  setState(() {
+                    isShowMore = !isShowMore;
+                  });
+                },
+                child: Text(
+                  isShowMore ? "Show more" : "Show less",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ))
+          ],
+        ),
       ),
     );
   }
