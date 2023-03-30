ff3f3511d4ba1f970f431d2f076c0c2944fc4b09
AbdoMady2002 <abdomady197@gmail.com>
2023-03-30 19:48:43 +0200

    add font style

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -1,4 +1,6 @@
 
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 import '../Shared/colors.dart';
@@ -12,11 +14,15 @@ class Home extends StatelessWidget {
         appBar: AppBar(
       actions: [
         Row(
-          children: [Text("\$13"), Text("\$13")],
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            Text("\$13", style: TextStyle(fontSize: 18)),
+            Text("\$13")
+          ],
         ),
       ],
       backgroundColor: appbarGreen,
-      title: const Text("Home"),
+      title: Text("Home"),
     ));
   }
 }
