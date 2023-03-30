a9fa94e170ffc4f11d1eab9a9578b51fc8a6f076
AbdoMady2002 <abdomady197@gmail.com>
2023-03-30 19:54:25 +0200

    add safe area + some changes

--- lib/Pages/Register.dart
+++ lib/Pages/Register.dart
@@ -10,31 +10,37 @@ class Register extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-      actions: [
-        Row(
-          children: [
-            Stack(
-              children: [
-                Container(
-                    child: Text("8"),
-                    decoration: BoxDecoration(
-                        color: Color.fromARGB(211, 164, 255, 193),
-                        shape: BoxShape.circle)),
-                IconButton(
-                    onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 12),
-              child: Text("\$13", style: TextStyle(fontSize: 18)),
-            ),
-          ],
-        ),
-      ],
-      backgroundColor: appbarGreen,
-      title: Text("Home"),
-    ));
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+        actions: [
+          Row(
+            children: [
+              Stack(
+                children: [
+                  Container(
+                      // ignore: sort_child_properties_last
+                      child: Text("8",
+                          style: TextStyle(
+                              fontSize: 18,
+                              color: Color.fromARGB(255, 0, 0, 0))),
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(211, 164, 255, 193),
+                          shape: BoxShape.circle)),
+                  IconButton(
+                      onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 12),
+                child: Text("\$13", style: TextStyle(fontSize: 18)),
+              ),
+            ],
+          ),
+        ],
+        backgroundColor: appbarGreen,
+        title: Text("Home"),
+      )),
+    );
   }
 }
