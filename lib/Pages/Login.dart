5eb524dd7cb35fe666667ff68ad562be3bde7b34
AbdoMady2002 <abdomady197@gmail.com>
2023-03-30 18:17:33 +0200

    add padding to login form

--- lib/Pages/Login.dart
+++ lib/Pages/Login.dart
@@ -8,21 +8,24 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      body: Center(child: Column(children: [
-        const SizedBox(height: 64,),
-        TextField(
-    keyboardType: TextInputType.text,
-    obscureText: true,
-    decoration: InputDecoration(
-      hintText: "Enter Your Password : ",
-      // To delete borders
-      enabledBorder: OutlineInputBorder(borderSide: Divider.createBorderSide(context),),
-      focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey,),),
-      // fillColor: Colors.red,
-      filled: true,
-      contentPadding: const EdgeInsets.all(8),
-    ))
-      ],
+      body: Center(child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(children: [
+          const SizedBox(height: 64,),
+          TextField(
+          keyboardType: TextInputType.text,
+          obscureText: true,
+          decoration: InputDecoration(
+        hintText: "Enter Your Password : ",
+        // To delete borders
+        enabledBorder: OutlineInputBorder(borderSide: Divider.createBorderSide(context),),
+        focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey,),),
+        // fillColor: Colors.red,
+        filled: true,
+        contentPadding: const EdgeInsets.all(8),
+          ))
+        ],
+        ),
       ),
       ),
     );
