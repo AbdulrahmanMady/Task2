c7fd95b49cec9f06e1ef372da737927cede65c4f
AbdoMady2002 <abdomady197@gmail.com>
2023-03-30 23:09:02 +0200

    add text to page

--- lib/Pages/Details_Screen.dart
+++ lib/Pages/Details_Screen.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
@@ -5,7 +8,9 @@ class Details extends StatelessWidget {
   const Details({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text(""),
+    );
   }
 }
