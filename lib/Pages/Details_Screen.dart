7e1e45a0438e224cd61f6325179b10e20ac78437
AbdoMady2002 <abdomady197@gmail.com>
2023-03-30 23:17:25 +0200

    add text to body + app bar

--- lib/Pages/Details_Screen.dart
+++ lib/Pages/Details_Screen.dart
@@ -4,13 +4,52 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
+import '../Shared/colors.dart';
+
 class Details extends StatelessWidget {
   const Details({super.key});
 
   @override
-Widget build(BuildContext context) {
+ Widget build(BuildContext context) {
     return Scaffold(
-      body: Text(""),
+      appBar: AppBar(
+        actions: [
+          Row(
+            children: [
+              Stack(
+                children: [
+                  Positioned(
+                    bottom: 24,
+                    child: Container(
+                        // ignore: sort_child_properties_last
+                        child: Text("8",
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: Color.fromARGB(255, 0, 0, 0))),
+                        decoration: BoxDecoration(
+                            color: Color.fromARGB(211, 164, 255, 193),
+                            shape: BoxShape.circle)),
+                  ),
+                  IconButton(
+                      onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 12),
+                child: Text("\$13", style: TextStyle(fontSize: 18)),
+              ),
+            ],
+          ),
+        ],
+        backgroundColor: appbarGreen,
+        title: Text("Home"),
+      ),
+      body: Column(
+        children: [
+          Image.asset("assets/img/1.webp"),
+          Text("\$ 12.99 "),
+        ],
+      ),
     );
   }
 }
