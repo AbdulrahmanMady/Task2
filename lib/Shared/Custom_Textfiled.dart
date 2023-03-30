38bee559acc7b0f46151e290ee396ced42f036a9
AbdoMady2002 <abdomady197@gmail.com>
2023-03-30 18:47:59 +0200

    add constins var in custom textfiled

--- lib/Shared/Custom_Textfiled.dart
+++ lib/Shared/Custom_Textfiled.dart
@@ -1,19 +1,28 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_constructors_in_immutables
 
 import 'package:flutter/material.dart';
 
 
 
 class MyTextFiled extends StatelessWidget {
-  const MyTextFiled({super.key});
+ final  TextInputType textInputTypee;
+ final  bool isPassword;
+ final  String hinttext;
+   MyTextFiled({super.key, 
+    required this.textInputTypee,
+    required this.isPassword,
+    required this.hinttext,
+
+
+   });
 
   @override
   Widget build(BuildContext context) {
     return TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  obscureText: false,
+                  keyboardType: textInputTypee,
+                  obscureText: isPassword,
                   decoration: InputDecoration(
-                    hintText: "Enter Your Email : ",
+                    hintText: hinttext,
                     // To delete borders
                     enabledBorder: OutlineInputBorder(
                       borderSide: Divider.createBorderSide(context),
