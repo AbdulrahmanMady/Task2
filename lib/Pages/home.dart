9eed80de35b2e7ccfbc7b91689bd6a0096087db7
AbdoMady2002 <abdomady197@gmail.com>
2023-03-30 21:46:12 +0200

    add child

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -8,7 +8,7 @@ import '../Shared/colors.dart';
 class Home extends StatelessWidget {
   const Home({super.key});
   @override
-   Widget build(BuildContext context) {
+  Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: GridView.builder(
@@ -19,11 +19,9 @@ class Home extends StatelessWidget {
                 mainAxisSpacing: 33),
             itemCount: 4,
             itemBuilder: (BuildContext context, int index) {
-              return Container(
-                color: Colors.grey,
-
-              );
-
+              return  GridTile(child: 
+              
+            );
             }),
           drawer: Drawer(
             // ignore: prefer_const_literals_to_create_immutables
@@ -63,38 +61,37 @@ class Home extends StatelessWidget {
           ]),
         ),
           appBar: AppBar(
-            actions: [
-              Row(
-                children: [
-                  Stack(
-                    children: [
-                      Positioned(
-                        bottom: 24,
-                        child: Container(
-                            // ignore: sort_child_properties_last
-                            child: Text("8",
-                                style: TextStyle(
-                                    fontSize: 18,
-                                    color: Color.fromARGB(255, 0, 0, 0))),
-                            decoration: BoxDecoration(
-                                color: Color.fromARGB(211, 164, 255, 193),
-                                shape: BoxShape.circle)),
-                      ),
-                      IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.add_shopping_cart)),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 12),
-                    child: Text("\$13", style: TextStyle(fontSize: 18)),
-                  ),
-                ],
-              ),
-            ],
-            backgroundColor: appbarGreen,
-            title: Text("Home"),
-          )),
+          actions: [
+            Row(
+              children: [
+                Stack(
+                  children: [
+                    Positioned(
+                      bottom: 24,
+                      child: Container(
+                          child: Text("8",
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  color: Color.fromARGB(255, 0, 0, 0))),
+                          decoration: BoxDecoration(
+                              color: Color.fromARGB(211, 164, 255, 193),
+                              shape: BoxShape.circle)),
+                    ),
+                    IconButton(
+                        onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 12),
+                  child: Text("\$13", style: TextStyle(fontSize: 18)),
+                ),
+              ],
+            ),
+          ],
+          backgroundColor: appbarGreen,
+          title: Text("Home"),
+        ),
+      ),
     );
   }
 }
