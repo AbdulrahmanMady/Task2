2ec0e910625a1f4dd050f4f4e59350396be2e283
AbdoMady2002 <abdomady197@gmail.com>
2023-03-30 18:14:07 +0200

    add text field to login form

--- lib/Pages/Login.dart
+++ lib/Pages/Login.dart
@@ -8,7 +8,19 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      body: Center(child: Column(children: [Text("data")
+      body: Center(child: Column(children: [
+        TextField(
+    keyboardType: TextInputType.text,
+    obscureText: true,
+    decoration: InputDecoration(
+      hintText: "Enter Your Password : ",
+      // To delete borders
+      enabledBorder: OutlineInputBorder(borderSide: Divider.createBorderSide(context),),
+      focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey,),),
+      // fillColor: Colors.red,
+      filled: true,
+      contentPadding: const EdgeInsets.all(8),
+    ))
       ],
       ),
       ),
