18bb40cd5a7b77e5f6ee8928fa9afc337d59a13a
AbdoMady2002 <abdomady197@gmail.com>
2023-03-30 19:25:48 +0200

    add edge to login

--- lib/Pages/Login.dart
+++ lib/Pages/Login.dart
@@ -13,54 +13,57 @@ class Login extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
           body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(
-              height: 70,
-            ),
-            TextField(
-                keyboardType: TextInputType.emailAddress,
-                obscureText: false,
-                decoration: widget_text_felid.copyWith(
-                  hintText: "Enter your email",
-                )),
-            const SizedBox(height: 30),
-            TextField(
-                keyboardType: TextInputType.text,
-                obscureText: true,
-                decoration: widget_text_felid.copyWith(
-                  hintText: "Enter your password",
-                )),
-            const SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: () {},
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(BTNgreen),
-                padding: MaterialStateProperty.all(const EdgeInsets.all(12)),
-                shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8))),
+        child: Padding(
+          padding: const EdgeInsets.all(33.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 70,
               ),
-              child: const Text(
-                "Sign in",
-                style: TextStyle(fontSize: 19),
+              TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  obscureText: false,
+                  decoration: widget_text_felid.copyWith(
+                    hintText: "Enter your email",
+                  )),
+              const SizedBox(height: 30),
+              TextField(
+                  keyboardType: TextInputType.text,
+                  obscureText: true,
+                  decoration: widget_text_felid.copyWith(
+                    hintText: "Enter your password",
+                  )),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(BTNgreen),
+                  padding: MaterialStateProperty.all(const EdgeInsets.all(12)),
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8))),
+                ),
+                child: const Text(
+                  "Sign in",
+                  style: TextStyle(fontSize: 19),
+                ),
               ),
-            ),
-            const SizedBox(height: 30),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("Do not have an account ?",
-                    style: TextStyle(fontSize: 20)),
-                TextButton(
-                    onPressed: () {},
-                    child: const Text(
-                      "ٍٍSign Up",
-                      style: TextStyle(color: Colors.black),
-                    ))
-              ],
-            )
-          ],
+              const SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Do not have an account ?",
+                      style: TextStyle(fontSize: 20)),
+                  TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "ٍٍSign Up",
+                        style: TextStyle(color: Colors.black),
+                      ))
+                ],
+              )
+            ],
+          ),
         ),
       )),
     );
